14c6f13db5477cee2f4257eff7699a8a4d7686be
ruby -v bash (curl -lS https://gist.githubusercontent.com/amadden80/2acf28ef193261602650/raw/3c6dc2cc44d88afca7b8feebb23b4bd72fe817d5/installfest.sh) <maitrikumar25@gmail.com>
2015-10-03 19:23:31 -0400

    d3 tips rendering

--- app/assets/javascripts/graph.js
+++ app/assets/javascripts/graph.js
@@ -25,9 +25,9 @@ function draw(jsonData){
 
   var billsData = jsonData;
   var field = d3.select('#graph');
-  // var margin = {top: 40, right: 20, bottom: 30, left: 40},
-  //     width = 960 - margin.left - margin.right,
-  //     height = 500 - margin.top - margin.bottom;
+  var margin = {top: 40, right: 20, bottom: 30, left: 40},
+      width = 575 - margin.left - margin.right,
+      height = 50 - margin.top - margin.bottom;
 
   field.selectAll('section')
        .data(billsData)
@@ -36,9 +36,9 @@ function draw(jsonData){
 
   field.selectAll('section')
        .data(billsData)
-       .text(function(bill){
-         return (bill.name);
-       });
+       .text(function(bill){return (bill.name);})
+       .attr("transform", "translate(" + -80 + "," + 0 + ")");    //move the center of the pie chart from 0, 0 to radius, radius
+
 
   field.selectAll('section')
        .data(billsData)
@@ -54,24 +54,38 @@ function draw(jsonData){
        .exit()
        .remove();
 
-   var tip = d3.tip()
-   .attr('class', 'd3-tip')
-   .offset([-10, 0])
-   .html(function(bill) {
-     return "<strong>Amount:</strong> <span style='color:deeppink'>" + bill.amount + "</span>";
-   })
+     var tip = d3.tip()
+       .attr('class', 'd3-tip')
+       .offset([10, -10])
+       .style("z-index", "10")
+       .html(function(d) {
+         return "<strong>Amount:</strong> <span style='color:deeppink'>" + d.amount + "</span>";
+       })
 
-  //  var svg = field.append("svg")
-  //   .attr("width", width + margin.left + margin.right)
-  //   .attr("height", height + margin.top + margin.bottom)
-  //   .append("g")
-  //   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+     var svg = d3.select("#graph").append("svg")
+                 .attr("width", width + margin.left + margin.right)
+                 .attr("height", height + margin.top + margin.bottom)
+                 .append("g")
+                 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-   field.call(tip);
+    //  var matrix = this.getScreenCTM()
+    //                   .translate(+this.getAttribute("cx"),
+    //                   +this.getAttribute("cy"));
+    // //You can use screen coordinates directly to position
+    // //a fixed-position tooltip
+    // tip
+    //     .style("left",
+    //            (matrix.e) + "px")
+    //     .style("top",
+    //            (matrix.f + 3) + "px");
+
+     svg.call(tip);
+
 
    field.selectAll('section')
 
       .data(billsData)
+      // .enter().append('section')
       .on('mouseover', tip.show)
       .on('mouseout', tip.hide)
 
@@ -100,50 +114,49 @@ function pie(jsonData){
       h = 230,                            //height
       r = Math.min(w, h) / 2,                            //radius
       labelr = r + 5, // radius for label anchor
-
-    color = d3.scale.category20c();     //builtin range of colors
+      color = d3.scale.category20c();     //builtin range of colors
     // color = d3.scale.ordinal()
     //     .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-    var vis = d3.select("#pie")
-        .append("svg:svg")              //create the SVG element inside the <body>
-        .data([billsData])                   //associate our data with the document
-            .attr("width", w+150)           //set the width and height of our visualization (these will be attributes of the <svg> tag
-            .attr("height", h+50)
-        .append("svg:g")                //make a group to hold our pie chart
-            .attr("transform", "translate(" + 190 + "," + 135 + ")")    //move the center of the pie chart from 0, 0 to radius, radius
-    var arc = d3.svg.arc()              //this will create <path> elements for us using arc data
-        .outerRadius(r)
-        .innerRadius(r-30);
-    var pie = d3.layout.pie()           //this will create arc data for us given a list of values
-        .value(function(d) { return d.amount; });    //we must tell it out to access the value of each element in our data array
-    var arcs = vis.selectAll("g.slice")     //this selects all <g> elements with class slice (there aren't any yet)
-        .data(pie)                          //associate the generated pie data (an array of arcs, each having startAngle, endAngle and value properties)
-        .enter()                            //this will create <g> elements for every "extra" data element that should be associated with a selection. The result is creating a <g> for every object in the data array
-            .append("svg:g")                //create a group to hold each slice (we will have a <path> and a <text> element associated with each slice)
+  var vis = d3.select("#pie")
+              .append("svg:svg")              //create the SVG element inside the <body>
+              .data([billsData])                   //associate our data with the document
+              .attr("width", w+150)           //set the width and height of our visualization (these will be attributes of the <svg> tag
+              .attr("height", h+50)
+              .append("svg:g")                //make a group to hold our pie chart
+              .attr("transform", "translate(" + 190 + "," + 135 + ")");    //move the center of the pie chart from 0, 0 to radius, radius
+  var arc = d3.svg.arc()              //this will create <path> elements for us using arc data
+              .outerRadius(r)
+              .innerRadius(r-30);
+  var pie = d3.layout.pie()           //this will create arc data for us given a list of values
+              .value(function(d) { return d.amount; });    //we must tell it out to access the value of each element in our data array
+  var arcs = vis.selectAll("g.slice")     //this selects all <g> elements with class slice (there aren't any yet)
+                .data(pie)                          //associate the generated pie data (an array of arcs, each having startAngle, endAngle and value properties)
+                .enter()                            //this will create <g> elements for every "extra" data element that should be associated with a selection. The result is creating a <g> for every object in the data array
+                .append("svg:g")                //create a group to hold each slice (we will have a <path> and a <text> element associated with each slice)
                 .attr("class", "slice");    //allow us to style things in the slices (like text)
-        arcs.append("svg:path")
-                .attr("fill", function(d, i) { return color(i); } ) //set the color for each slice to be chosen from the color function defined above
-                .transition()
-                .duration(3000)
-                .attr("d", arc);                                    //this creates the actual SVG path using the associated data (pie) with the arc drawing function
 
+      arcs.append("svg:path")
+          .attr("fill", function(d, i) { return color(i); } ) //set the color for each slice to be chosen from the color function defined above
+          .attr("d", arc)                                   //this creates the actual SVG path using the associated data (pie) with the arc drawing function
+          .transition()
+          .duration(3000);
 
-        arcs.append("svg:text")                                     //add a label to each slice
-        .attr("transform", function(d) {
-                var c = arc.centroid(d),
-                    x = c[0],
-                    y = c[1],
-                    // pythagorean theorem for hypotenuse
-                    h = Math.sqrt(x*x + y*y);
-                return "translate(" + (x/h * labelr) +  ',' +
-                   (y/h * labelr) +  ")";
-            })
-            .attr("dy", ".35em")
-            .attr("text-anchor", function(d) {
-                // are we past the center?
-                return (d.endAngle + d.startAngle)/2 > Math.PI ?
-                    "end" : "start";
-            })
-            .text(function(d, i) { return billsData[i].name; });
+      arcs.append("svg:text")                                     //add a label to each slice
+          .attr("transform", function(d) {
+              var c = arc.centroid(d),
+                  x = c[0],
+                  y = c[1],
+                  // pythagorean theorem for hypotenuse
+                  h = Math.sqrt(x*x + y*y);
+              return "translate(" + (x/h * labelr) +  ',' +
+                 (y/h * labelr) +  ")";
+          })
+          .attr("dy", ".35em")
+          .attr("text-anchor", function(d) {
+              // are we past the center?
+              return (d.endAngle + d.startAngle)/2 > Math.PI ?
+                  "end" : "start";
+          })
+          .text(function(d, i) { return billsData[i].name; });
 
-          }
+        }
